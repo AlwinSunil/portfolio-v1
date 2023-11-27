bb706f72454e9af371f50521676b60f04d942a35
Alwin Sunil <64636626+AlwinSunil@users.noreply.github.com>
2023-11-27 15:54:30 +0530

    Removed last 14 commits to one, Fixed bugs

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,6 @@ var firebaseConfig = {
   apiKey: "AIzaSyDxXjOKHmWQ0X8MX_9p_5vdY1iJEuAb-Os",
   authDomain: "portfolio-c0794.firebaseapp.com",
   projectId: "portfolio-c0794",
-  storageBucket: "portfolio-c0794.appspot.com",
   messagingSenderId: "678660982903",
   appId: "1:678660982903:web:f1772be5bb8e45918a1e72",
 };
@@ -11,7 +10,6 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-var db = firebase.firestore();
 
 window.onload = function () {
   window.scrollTo(0, 0);
@@ -20,57 +18,57 @@ window.onload = function () {
   document.getElementById("body").style.height = "auto";
   document.getElementById("body").style.overflow = "auto";
   document.getElementById("body").style.overflowX = "hidden";
-  firebaseQuote();
-};
+}
+
+// Nav Active
+var btns = $("#navigation .nav-item .nav-link");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click",
+    function () {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+}
+
+// Code for changing active link on Scrolling
+$(window).scroll(function () {
+  var distance = $(window).scrollTop();
+  $('.page-section').each(function (i) {
+    if ($(this).position().top
+      <= distance + 200) {
+      $('.nav-items a.active').removeClass('active');
+      $('.nav-items a').eq(i).addClass('active');
+    }
+  });
+}).scroll();
+
+$('.nav-item a').click(function () {
+  $(this).addClass('active');
+  $('a.active').not(this).removeClass('active');
+});
 
 // Scroll To Top
 var footerLogo = document.getElementById("footer-logo");
-
 footerLogo.onclick = function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
 
-//h4 Hero Component
+// h4 Hero Component
 var h4Width = document.getElementById("inner-wrapper").offsetWidth;
 var h4DivWidth = h4Width;
-
 document.getElementById("h4-hero").style.width = h4DivWidth + "px";
 
-// Hightlight Element
+// Highlight Element
 var nameWidth = document.getElementById("name-link").offsetWidth;
 var highlightWidth = nameWidth + (15 / 100) * nameWidth;
-
 document.getElementById("highlight").style.width = highlightWidth + "px";
-document.getElementById("highlight-footer").style.width = highlightWidth + "px";
 
-// Function to Dynamically Change Quote
-function firebaseQuote() {
-  const totalDoc = db
-    .collection("quotes")
-    .get()
-    .then(function (querySnapshot) {
-      var totalDoc = querySnapshot.size;
-
-      var randomNum = Math.floor(Math.random() * totalDoc + 1);
-      var quoteID = "quote-" + randomNum;
-      var docRef = db.collection("quotes").doc(quoteID);
-
-      docRef
-        .get()
-        .then((doc) => {
-          if (doc.exists) {
-            var obj = doc.data();
-            var quoteContent = document.getElementById("quote-content");
-            var quotePerson = document.getElementById("quote-person");
-            quoteContent.innerHTML = obj.content;
-            quotePerson.innerHTML = obj.person;
-          } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-          }
-        })
-        .catch((error) => {
-          console.log("Error getting document:", error);
-        });
-    });
+var windowWidth = window.screen.width;
+if (windowWidth > 700) {
+  var highlightWidthFooter = nameWidth + (-3 / 100) * nameWidth;
+  document.getElementById("highlight-footer").style.width = highlightWidthFooter + "px";
+} else {
+  var highlightWidthFooter = nameWidth + (15 / 100) * nameWidth;
+  document.getElementById("highlight-footer").style.width = highlightWidthFooter + "px";
 }
